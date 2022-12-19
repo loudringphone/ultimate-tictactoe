554bafb414c6d1d516c5e262ec3e4e88238a0547
Wing Fung Lau <wingfunglau@192-168-1-101.tpgi.com.au>
2022-12-19 15:56:58 +1100

    countdown random rematch result done

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let  cells = document.querySelectorAll('.cell');
 let markXs = document.querySelectorAll('.markX');
 let markOs = document.querySelectorAll('.markO');
 const body = document.querySelector('body')
+const timer = document.querySelector('.timer')
+
 
 let cons2Win = [
 [0, 1, 2],
@@ -66,10 +68,12 @@ characters.forEach(target => {
             charContainer.classList.toggle('change');
             charContainer.style.visibility = 'hidden';    
             mainContent.style.visibility = 'visible';
+            playerNames[0].id = 'gameStarted'
         }
         })
     })
 }
+
 
 
 
@@ -106,6 +110,7 @@ const congrats = function() {
     })
     startScreen.style.top = '60%';
     startScreen.style.opacity = 1;
+    playerNames[0].removeAttribute('id');
 }
  
 
@@ -135,8 +140,8 @@ const winningText = function(winner) {
 
 cells.forEach(target =>
     target.textContent = 'X')
-let player1 = []
-let player2 = []
+let p1played = []
+let p2played = []
 let checkP1 = "";
 let checkP2 = "";
 const scores = document.querySelectorAll('.score')
@@ -146,6 +151,8 @@ for (let cell of cells) {
         if (cell.className === 'markX' || cell.className === 'markO') {
             return
         }
+
+        timer.textContent = 5;
 
         markXs = document.querySelectorAll('.markX');
         markOs = document.querySelectorAll('.markO');
@@ -157,7 +164,7 @@ for (let cell of cells) {
                 empty.textContent = "〇"
                 empty.classList.toggle('cell2')
             }
-            player1.push(parseInt(cell.id))
+            p1played.push(parseInt(cell.id))
 
  
         }
@@ -170,59 +177,148 @@ for (let cell of cells) {
                 empty.textContent = "X"
                 empty.classList.toggle('cell2')
             }
-            player2.push(parseInt(cell.id))
-        }
-    
-
-        for (let i = 0; i < cons2Win.length; i++) {
-            checkP1 = ""
-            checkP2 = ""
-            for (let j of cons2Win[i]){
-                if (player1.includes(j)) {
-                    checkP1 = checkP1 + 'W'
-                    if (checkP1.length === 3) {
-                        empties = document.querySelectorAll('.cell2');
-                        empties.forEach(target => {
-                            target.textContent = "";
-                            target.classList.add('markX');
-                            target.classList.remove('cell');
-                            target.classList.remove('cell2');
-                        });
-                        congratsText.textContent = winningText(playerNames[0].textContent)
-                        scores[0].textContent = `Wins: ${parseInt(scores[0].textContent.split(" ")[1]) + 1}`
-                        players[1].style.filter = "grayscale(100%)";
-                        congrats()
-                        
-                        victor = playerNames[0].textContent
-
-                        return
-
-                    }
-                }
-                if (player2.includes(j)) {
-                    checkP2 = checkP2 + 'W'
-                    if (checkP2.length === 3) {
-                        empties = document.querySelectorAll('.cell');
-                        empties.forEach(target => {
-                            target.textContent = "";
-                            target.classList.add('markO');
-                            target.classList.remove('cell');
-                        });
-                        victor = playerNames[1];
-                        congratsText.textContent = winningText(playerNames[1].textContent)
-                        players[0].style.filter = "grayscale(100%)";
-                        congrats()
-
-                        victor = playerNames[1].textContent
-
-                        return
-                    }
-                }
-            }
+            p2played.push(parseInt(cell.id))
         }
     });
 }
 }
+
+
+
+// Timer
+const countDown = function() {
+    if (playerNames[0].id === 'gameStarted') {
+        timer.textContent = parseInt(timer.textContent) - 1}
+}
+
+
+
+let countDownInt = setInterval(countDown , 1000);
+
+
+
+//A random move is made when time is up
+
+
+
+const timeCheck = function() {
+    if (parseInt(timer.textContent) < 0 && victor != playerNames[0].textContent && victor != playerNames[1].textContent) {
+        timer.textContent = 5;
+        cells = document.querySelectorAll('.cell');
+        let i = Math.floor(Math.random() * cells.length);
+        try {
+            if (cells[i].textContent === 'X') {
+                cells[i].classList.add('markX');
+                cells[i].classList.remove('cell');
+                empties = document.querySelectorAll('.cell');
+                for (let empty of empties) {
+                    empty.textContent = "〇";
+                    empty.classList.toggle('cell2');
+                }
+            p1played.push(parseInt(cells[i].id));
+            console.log(p1played);
+            } else {
+                cells[i].classList.add('markO')
+                cells[i].classList.remove('cell');
+                cells[i].classList.remove('cell2');
+                empties = document.querySelectorAll('.cell');
+                for (let empty of empties) {
+                    empty.textContent = "X";
+                    empty.classList.toggle('cell2');
+                }
+                p2played.push(parseInt(cells[i].id));
+        
+            console.log(p2played);
+            }
+        }
+        catch {};
+    }
+}
+
+
+let timeCheckInt = setInterval(timeCheck, 100);
+
+
+
+const winCheck = function() {
+    for (let i = 0; i < cons2Win.length; i++) {
+        checkP1 = ""
+        checkP2 = ""
+        for (let j of cons2Win[i]){
+            if (p1played.includes(j)) {
+                checkP1 = checkP1 + 'W'
+                if (checkP1.length === 3) {
+                    empties = document.querySelectorAll('.cell2');
+                    empties.forEach(target => {
+                        target.textContent = "";
+                        target.classList.add('markX');
+                        target.classList.remove('cell');
+                        target.classList.remove('cell2');
+                    });
+                    congratsText.textContent = winningText(playerNames[0].textContent)
+                    scores[0].textContent = `Wins: ${parseInt(scores[0].textContent.split(" ")[1]) + 1}`
+                    players[1].style.filter = "grayscale(100%)";
+                    congrats()
+                    
+                    victor = playerNames[0].textContent
+                    clearInterval(winCheckInt)
+                    clearInterval(countDownInt)
+                    clearInterval(timeCheckInt)
+                }
+            }
+            if (p2played.includes(j)) {
+                checkP2 = checkP2 + 'W'
+                if (checkP2.length === 3) {
+                    empties = document.querySelectorAll('.cell');
+                    empties.forEach(target => {
+                        target.textContent = "";
+                        target.classList.add('markO');
+                        target.classList.remove('cell');
+                    });
+                    victor = playerNames[1];
+                    congratsText.textContent = winningText(playerNames[1].textContent)
+                    players[0].style.filter = "grayscale(100%)";
+                    congrats()
+    
+                    victor = playerNames[1].textContent
+                    clearInterval(winCheckInt)
+                    clearInterval(countDownInt)
+                    clearInterval(timeCheckInt)
+                }
+            }
+        }
+    }
+
+
+
+    // console.log(player1.length)
+    // console.log(player2.length)  total:9
+    if (p1played.length + p2played.length === 9 && checkP1.length != 3 && checkP2.length != 3) {
+        congratsText.textContent = 'Draw!'
+        players[0].style.filter = "grayscale(100%)";
+        players[1].style.filter = "grayscale(100%)";
+        congrats()
+        victor = playerNames[Math.floor(Math.random())*2].textContent
+        clearInterval(winCheckInt)
+        clearInterval(countDownInt)
+        clearInterval(timeCheckInt)
+
+
+    }
+
+
+
+
+
+
+}
+
+
+let winCheckInt = setInterval(winCheck, 100);
+
+
+
+
 
 
 //Rematch
@@ -270,8 +366,8 @@ const rematch = function() {
     setTimeout(function(){
         checkP1 = "";
         checkP2 = "";
-        player1 = [];
-        player2 = [];
+        p1played = [];
+        p2played = [];
 
         markXs.forEach(target =>
             target.classList.add('cell'));
@@ -295,8 +391,13 @@ const rematch = function() {
 
         victor = "";
         mainContent.style.visibility = 'visible';
+        playerNames[0].id = 'gameStarted'
+        countDownInt = setInterval(countDown , 1000);
+        timeCheckInt = setInterval(timeCheck, 100);
+        winCheckInt = setInterval(winCheck, 100);
     }, 3000)
 }
+
 
 CharSel()
 
